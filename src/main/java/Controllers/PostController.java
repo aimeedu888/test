d9e2db8f6692ddb2b86da7aa108b97cfc4de3986
Roy Choi <rschoi@usc.edu>
2023-11-26 00:06:55 -0800

    Added old files

---
+++ src/main/java/Controllers/PostController.java
@@ -0,0 +1,6 @@
+package Controllers;
+import com.heroku.java.Objects.Post;
+import Repositories.PostRepository;
+public class PostController {
+	// add api routes for CRUD operations revolving around Posts
+}
---
+++ src/main/java/com/heroku/java/Objects/User.java
@@ -0,0 +1,66 @@
+package com.heroku.java.Objects;
+
+import java.util.HashMap;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	private HashMap<String, Post> userPost; // Assuming Post is another class, adjust the type accordingly
+	private String username_;
+	private String password_;
+	private String email_;
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int userID_;
+
+	public User(String username, String password, String email) {
+		this.username_ = username;
+		this.password_ = password;
+		this.email_ = email;
+		this.userPost = new HashMap<>();
+	}
+
+	public HashMap<String, Post> getUserPost() {
+		return userPost;
+	}
+
+	public void setUserPost(HashMap<String, Post> userPost) {
+		this.userPost = userPost;
+	}
+
+	public String getUsername() {
+		return username_;
+	}
+
+	public void setUsername(String username) {
+		this.username_ = username;
+	}
+
+	public String getPassword() {
+		return password_;
+	}
+
+	public void setPassword(String password) {
+		this.password_ = password;
+	}
+
+	public String getEmail() {
+		return email_;
+	}
+
+	public void setEmail(String email) {
+		this.email_ = email;
+	}
+
+	public int getUserID() {
+		return userID_;
+	}
+
+	public void setUserID(int userID) {
+		this.userID_ = userID;
+	}
+}

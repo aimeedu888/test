5a50f0326e11423bcc11da02ea9198d3a77332fb
Guangda Fei <guangdaf@usc.edu>
2023-11-27 13:03:22 -0800

    11.27.13.03

--- src/main/java/com/heroku/java/Controllers/PostController.java
+++ src/main/java/com/heroku/java/Controllers/PostController.java
@@ -1,6 +1,79 @@
 package com.heroku.java.Controllers;
-import com.heroku.java.Objects.Post;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+import com.heroku.java.Objects.*;
 import com.heroku.java.Repositories.*;
+
+@RestController
+@RequestMapping("/api/post")
+@CrossOrigin
 public class PostController {
-	// add api routes for CRUD operations revolving around Posts
+	
+	private PostRepository postRepository;
+	@Autowired
+	public PostController(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
+	
+	@GetMapping("/api/post/getAllPost")
+	public @ResponseBody Iterable<Post> getAllPosts() {
+		return postRepository.findAll();
+	}
+	
+	@GetMapping("/api/post/getPostByUser")
+	public @ResponseBody List<Post> getPostsByUser(int user_id) {
+		Iterable<Post> allpost = postRepository.findAll();
+		List<Post> posts = new ArrayList<Post>();
+		for (Post p : allpost) {
+			if (p.getUser_id()== user_id) {
+				posts.add(p);
+			}
+		}
+		return posts;
+	}
+	
+	@GetMapping("/api/post/getPostByID")
+	public @ResponseBody Optional<Post> getPostsByID(int post_id) {
+		return postRepository.findById(post_id);
+	}
+
+	@PostMapping("/api/post/addPost")
+	public @ResponseBody Integer addPost(@RequestParam String postTitle, @RequestParam String imageUrl,
+			@RequestParam Double itemPrice, @RequestParam String description, @RequestParam String seller, 
+			@RequestParam Integer user_id, @RequestParam Boolean sold) {
+		for (Post t : postRepository.findAll()) {
+			if (t.getPostTitle().equals(postTitle)) {
+				System.out.println("Duplicate postTitle, cannot add Post");
+				return -1;
+			}
+		}
+		Post p = new Post(postTitle,imageUrl, itemPrice, description,seller, sold, user_id, 0);
+		postRepository.save(p);
+		return p.getPostID();
+	}
+	
+    @DeleteMapping("/api/user/deletePost")
+    public @ResponseBody String deleteUser(@RequestParam Integer postID) {
+        if (postRepository.existsById(postID)) {
+            postRepository.deleteById(postID);
+            return "Post with ID " + postID + " deleted successfully";
+        } else {
+            return "Post with ID " + postID + " not found, cannot delete";
+        }
+    }
 }
--- src/main/java/com/heroku/java/Controllers/UserController.java
+++ src/main/java/com/heroku/java/Controllers/UserController.java
@@ -1,53 +1,68 @@
 package com.heroku.java.Controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.heroku.java.Objects.User;
 import com.heroku.java.Repositories.*;
+
+
 @RestController
 @RequestMapping("/api/user")
 @CrossOrigin
 public class UserController {
+	
+	private UserRepository userRepository;
+	@Autowired
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
-    @Autowired
-    private UserRepository userRepository;
+	@GetMapping("/api/user/getUser")
+	public @ResponseBody Iterable<User> getAllUsers(@RequestParam Integer ID) {
+		return userRepository.findAll();
+	}
+	
+	@GetMapping("/api/user/getUserByID")
+	public @ResponseBody Optional<User> getUsers(@RequestParam Integer ID) {
+		return userRepository.findById(ID);
+	}
 
-    @GetMapping("/getAllUsers")
-    public Iterable<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-
-    @PostMapping("/addUser")
-    public Integer addUser(@RequestParam String username, @RequestParam String password, @RequestParam String email) {
-        for (User t : userRepository.findAll()) {
-            if (t.getUsername().equals(username)) {
-                System.out.println("Duplicate username, cannot add user");
-                return -1;
-            }
-            if (t.getEmail().equals(email)) {
-                System.out.println("Duplicate email, cannot add user");
-                return -1;
-            }
-        }
-        User u = new User(username, password, email);
-        userRepository.save(u);
-        return u.getUserID();
-    }
-
-    @GetMapping("/validateUser")
-    public User validateUser(@RequestParam String username, @RequestParam String password) {
-        User user = userRepository.findByUsername(username);
-        if (user != null && user.getPassword().equals(password)) {
-            return user;
-        } else {
-            System.out.println("Authentication failed for user: " + username);
-            return null;
-        }
-    }
-
-    @DeleteMapping("/deleteUser")
-    public String deleteUser(@RequestParam Integer userID) {
+	@PostMapping("/api/user/addUser")
+	public @ResponseBody Integer addUser(@RequestParam String username, @RequestParam String password,
+			@RequestParam String email) {
+		for (User t : userRepository.findAll()) {
+			if (t.getUsername().equals(username)) {
+				System.out.println("Duplicate username, cannot add user");
+				return -1;
+			}
+			if (t.getEmail().equals(email)) {
+				System.out.println("Duplicate email, cannot add user");
+				return -1;
+			}
+		}
+		User u = new User(username, password, email);
+		userRepository.save(u);
+		return u.getUserID();
+	}
+	@GetMapping("/api/user/validateUser")
+	public @ResponseBody User validateUser(@RequestParam String username, @RequestParam String password) {
+		 User user = userRepository.findByUsername(username);
+		    if (user != null) {
+		        if (user.getPassword().equals(password)) {
+		            return user;
+		        } else {
+		            System.out.println("Incorrect password for user: " + username);
+		        }
+		    } else {
+		        System.out.println("User not found: " + username);
+		    }
+		    return null;
+	}
+    @DeleteMapping("/api/user/deleteUser")
+    public @ResponseBody String deleteUser(@RequestParam Integer userID) {
         if (userRepository.existsById(userID)) {
             userRepository.deleteById(userID);
             return "User with ID " + userID + " deleted successfully";

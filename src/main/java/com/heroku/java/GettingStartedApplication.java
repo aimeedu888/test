6c4e4806f3b6b832af11c242afd2807e734dfe80
Guangda Fei <guangdaf@usc.edu>
2023-11-27 22:28:00 -0800

    11.27.22.27 all css and js are in the right position

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -25,33 +25,84 @@ public class GettingStartedApplication {
     public String loginhome() {
         return "login-registeration/login";
     }
+    
+    //home page
     @GetMapping("/homepage.html")
     public String home() {
         return "homepage";
     }
+    @GetMapping("homepage.css")
+    public String homepage_css() {
+        return "css/homepage";
+    }
+    @GetMapping("/homepage.js")
+    public String homepage_js() {
+        return "js/homepage";
+    }
     
+    //login
     @GetMapping("/login-registeration/login.html")
     public String login() {
         return "login-registeration/login";
     }
-    @GetMapping("/login-registeration/css/login.css")
+    @GetMapping("/login-registeration/login.css")
     public String login_css() {
         return "css/login";
     }
     @GetMapping("/login-registeration/login.js")
     public String login_js() {
-        return "login-registeration/login.js";
+        return "js/login";
+    }
+    @GetMapping("/login-registeration/login-register.css")
+    public String login_register_js() {
+        return "css/login-register";
+    }
+    @GetMapping("/login-registeration/login-register-api.js")
+    public String login_reg_api_js() {
+        return "js/login-register-api";
+    }
+    @GetMapping("/login-registeration/margins.css")
+    public String margins_css() {
+        return "css/margins";
     }
     
+    //register
     @GetMapping("/login-registeration/register.html")
     public String register() {
         return "login-registeration/register";
     }
+    @GetMapping("/login-registeration/register.css")
+    public String register_css() {
+        return "css/margins";
+    }
+    @GetMapping("/login-registeration/register.js")
+    public String register_js() {
+        return "js/register";
+    }
+    
+    //closet
     @GetMapping("/closet.html")
     public String closet() {
         return "closet";
     }
+    @GetMapping("/closet.css")
+    public String closet_css() {
+        return "css/closet";
+    }
+    @GetMapping("/closet.js")
+    public String closet_js() {
+        return "js/closet";
+    }
     
+    //API
+    @GetMapping("/login-registeration/api.js")
+    public String api_js1() {
+        return "js/api";
+    }
+    @GetMapping("/api.js")
+    public String api_js2() {
+        return "js/api";
+    }
     
 
     @GetMapping("/database")

f4e996011bcc4bc55c95e62be1e8e8f206811a84
Guangda Fei <guangdaf@usc.edu>
2023-11-29 11:42:19 -0800

    11.29.11:42

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -143,27 +143,27 @@ public class GettingStartedApplication {
     }
     
 
-//    @GetMapping("/database")
-//    String database(Map<String, Object> model) {
-//        try (Connection connection = dataSource.getConnection()) {
-//            final var statement = connection.createStatement();
-//            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-//            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
-//
-//            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
-//            final var output = new ArrayList<>();
-//            while (resultSet.next()) {
-//                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
-//            }
-//
-//            model.put("records", output);
-//            return "database";
-//
-//        } catch (Throwable t) {
-//            model.put("message", t.getMessage());
-//            return "error";
-//        }
-//    }
+    @GetMapping("/database")
+    String database(Map<String, Object> model) {
+        try (Connection connection = dataSource.getConnection()) {
+            final var statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            statement.executeUpdate("INSERT INTO ticks VALUES (now())");
+
+            final var resultSet = statement.executeQuery("SELECT tick FROM ticks");
+            final var output = new ArrayList<>();
+            while (resultSet.next()) {
+                output.add("Read from DB: " + resultSet.getTimestamp("tick"));
+            }
+
+            model.put("records", output);
+            return "database";
+
+        } catch (Throwable t) {
+            model.put("message", t.getMessage());
+            return "error";
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GettingStartedApplication.class, args);

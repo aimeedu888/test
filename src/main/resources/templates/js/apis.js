be4e7dd9f6b4267e694e2187a8e90c5a12dc381d
Guangda Fei <guangdaf@usc.edu>
2023-11-29 15:06:35 -0800

    11.29.15:06

--- src/main/resources/templates/js/apis.js
+++ src/main/resources/templates/js/apis.js
@@ -92,116 +92,101 @@ function addPost(postTitle, imageUrl_, itemPrice_, description_,sold_,user_id){
 	  });
 }
 
-function deletePostByID(postId){
-	return fetch('/api/post/deletePost?postID='+postId, {
-	  method: 'DELETE',
-	  headers: {
-	    'Content-Type': 'text/plain',
-	    'Access-Control-Allow-Origin': '*',
-	  },
-	})
-	.then(response => response.text())
-	  .then(data => {
-	    console.log('api.js deletePost: \n returns:' + data);
-	    // Handle success response from the server
-	  })
-	  .catch((error) => {
-	    console.error('Error:', error);
-	    // Handle error
-	  });
+async function deletePostByID(postId){
+	try {
+        const response = await fetch('/api/post/deletePost?postID=' + postId, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+        const data = await response.text();
+        console.log('api.js deletePost: \n returns:' + data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
-function test(xxx, xxx2)
+async function test(xxx, xxx2)
 {
 	const sending = {
 		"test1": xxx,
 		"test2": "okkkk"
 	}; // this does not matter
-	return fetch('/api/post/PostTest?test1='+xxx+'&test2='+xxx2, {
-	  method: 'POST',
-	  headers: {
-	    'Content-Type': 'text/plain',
-	    'Access-Control-Allow-Origin': '*',
-	  },
-	  body: JSON.stringify(sending)
-	})
-	  .then(response => response.text())
-	  .then(data => {
-	    console.log(data);
-	    // Handle success response from the server
-	  })
-	  .catch((error) => {
-	    console.error('Error:', error);
-	    // Handle error
-	  });
+	try {
+        const response = await fetch('/api/post/PostTest?test1=' + xxx + '&test2=' + xxx2, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify(sending)
+        });
+        const data = await response.text();
+        console.log(data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 
 
 // UserController
 
-function getAllUsers ()
+async function getAllUsers ()
 {
-	return fetch('/api/user/getAllUsers', {
-	  method: 'GET',
-	  headers: {
-	    'Content-Type': 'application/json',
-	  }
-	})
-	  .then(response => response.json())
-	  .then(data => {
-	    console.log('api.js getAllUsers: need implementation, ' +
-	    'data should be a json array with all posts: \n\n' + data.tostring());
-	    // Handle success response from the server
-	  })
-	  .catch((error) => {
-	    console.error('Error:', error);
-	    // Handle error
-	  });
+	try {
+        const response = await fetch('/api/user/getAllUsers', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+        const data = await response.json();
+        console.log('api.js getAllUsers: need implementation, ' +
+            'data should be a json array with all posts: \n\n' + data.tostring());
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
-function getUsers(ID){
-	return fetch('/api/user/getUserByID?ID='+ID, {
-	  method: 'GET',
-	  headers: {
-	    'Content-Type': 'application/json',
-	    'Access-Control-Allow-Origin': '*',
-	  },
-	  body: JSON.stringify(sending)
-	})
-	.then(response => response.json())
-	  .then(data => {
-	    console.log('api.js getUsers: need implementation, ' +
-	    'data should be a json of the post: \n\n' + data.tostring());
-	    // Handle success response from the server
-	  })
-	  .catch((error) => {
-	    console.error('Error:', error);
-	    // Handle error
-	  });
+async function getUsers(ID){
+	try {
+        const response = await fetch('/api/user/getUserByID?ID=' + ID, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify(sending)
+        });
+        const data = await response.json();
+        console.log('api.js getUsers: need implementation, ' +
+            'data should be a json of the post: \n\n' + data.tostring());
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
-function addUser(username, password_, email_){
+async function addUser(username, password_, email_){
 	
 	para = '?username='+username+'&password='+password_+'&email='+email_;
 	
-	return fetch('/api/user/addUser'+para, {
-	  method: 'POST',
-	  headers: {
-	   'Content-Type': 'text/plain',
-	    'Access-Control-Allow-Origin': '*',
-	  },
-	})
-	.then(response => response.text())
-	  .then(data => {
-	    console.log('api.js addUser: returns the id of the added user:' + data);
-	    return data;
-	    // Handle success response from the server
-	  })
-	  .catch((error) => {
-	    console.error('Error:', error);
-	    // Handle error
-	  });
+	try {
+        const response = await fetch('/api/user/addUser' + para, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+        const data = await response.text();
+        console.log('api.js addUser: returns the id of the added user:' + data);
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 async function validateUser(username, password_){
@@ -225,23 +210,20 @@ async function validateUser(username, password_){
     }
 }
 
-function deleteUser(userID){
-	return fetch('/api/post/deletePost?userID='+userID, {
-	  method: 'DELETE',
-	  headers: {
-	    'Content-Type': 'text/plain',
-	    'Access-Control-Allow-Origin': '*',
-	  },
-	})
-	.then(response => response.text())
-	  .then(data => {
-	    console.log('api.js deleteUser: \n returns a message:' + data);
-	    // Handle success response from the server
-	  })
-	  .catch((error) => {
-	    console.error('Error:', error);
-	    // Handle error
-	  });
+async function deleteUser(userID){
+	try {
+        const response = await fetch('/api/post/deletePost?userID=' + userID, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Origin': '*',
+            },
+        });
+        const data = await response.text();
+        console.log('api.js deleteUser: \n returns a message:' + data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
 
 

95c11ab03eaff49f00ed81529e92e6cebf0a2cfe
Roy Choi <rschoi@usc.edu>
2023-11-29 22:49:08 -0800

    c

--- src/main/resources/templates/js/login.js
+++ src/main/resources/templates/js/login.js
@@ -1,64 +1,65 @@
 const clearErrorMsgs = () => {
-  const errorMsgs = document.getElementsByTagName("small");
-  for (var i = 0; i < errorMsgs.length; i++) {
-    errorMsgs[i].style.visibility = "hidden";
-  }
+	const errorMsgs = document.getElementsByTagName("small");
+	for (var i = 0; i < errorMsgs.length; i++) {
+		errorMsgs[i].style.visibility = "hidden";
+	}
 }
 
-document.querySelector('#login-form').onsubmit = async () => {
-  console.log("Attempting to submit login form");
-  clearErrorMsgs();
-  const username = document.getElementById("username");
-  const password = document.getElementById("password");
+document.querySelector('#login-form').onsubmit = async (e) => {
+	e.preventDefault();
+	console.log("Attempting to submit login form");
+	clearErrorMsgs();
+	const username = document.getElementById("username");
+	const password = document.getElementById("password");
 
-  const userError = document.getElementById("userError");
-  const passError = document.getElementById("passError");
-  const authError = document.getElementById("authError");
+	const userError = document.getElementById("userError");
+	const passError = document.getElementById("passError");
+	const authError = document.getElementById("authError");
 
-  var isFilled = true;
-  if (username.value.trim() == "") {
-    userError.style.visibility = "visible";
-    isFilled = false;
-  }
-  if (password.value.trim() == "") {
-    passError.style.visibility = "visible";
-    isFilled = false;
-  }
-  if (isFilled) {
-	  const temp = await autheticate(username.value, password.value)
-    if (temp) {
-		console.log("<<<<<<<true");
-      //ADD LOGIC TO GO TO MAIN HOME PAGE
-      return true;
-    }
-    else {
-      authError.style.visibility = "visible";
-      console.log("<<<<<<<false");
-      return false;
-    }
-  }
-  else {
-    return false;
-  }
+	var isFilled = true;
+	if (username.value.trim() == "") {
+		userError.style.visibility = "visible";
+		isFilled = false;
+	}
+	if (password.value.trim() == "") {
+		passError.style.visibility = "visible";
+		isFilled = false;
+	}
+	if (isFilled) {
+		const temp = await autheticate(username.value, password.value)
+		if (temp) {
+			console.log("<<<<<<<true");
+			//ADD LOGIC TO GO TO MAIN HOME PAGE
+			return true;
+		}
+		else {
+			authError.style.visibility = "visible";
+			console.log("<<<<<<<false");
+			return false;
+		}
+	}
+	else {
+		return false;
+	}
 
 }
 
- async function autheticate(username, password){
-  console.log("Autheticating username and password combination");
-  //CALL API FUNCTION HERE
-  const temp =  await validateUser(username, password);
-   // 1 means found, 0 means username not found, -1 means password incorrect
-   console.log("autheticate: temp is: "+temp);
-  if (temp == "1"){
-	  console.log("autheticate: 1");
-	  return false;
-  }
-  else{
-	  console.log("autheticate: 2");
-	  return false;
-  }
+async function autheticate(username, password) {
+	console.log("Autheticating username and password combination");
+	//CALL API FUNCTION HERE
+	const temp = await validateUser(username, password);
+	// 1 means found, 0 means username not found, -1 means password incorrect
+	console.log("autheticate: temp is: " + temp);
+	if (temp == "1") {
+		console.log("autheticate: 1");
+		return false;
+	}
+	else {
+		console.log("autheticate: 2");
+		return false;
+	}
 }
 
 function setLocalStorage() {
-  localStorage.setItem("currentUser", -1);
+	localStorage.setItem("currentUser", -1);
 }
